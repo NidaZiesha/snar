c6e44f4a51ef755404a14896f0c83873a34d57b5
ammaar8 <asketch8@gmail.com>
2018-06-27 15:43:46 +0530

    Added _weapons and _enemies.
    These contain pre made weapons and enemies for usage in game scenes.

---
+++ _enemies.py
@@ -0,0 +1,9 @@
+from Character import Enemy
+
+# Low levelled enemies 0 - 10 dmg
+# For forest tutorial Give fancy names later.
+spider = Enemy('Spider', 20, (2,5))
+bug = Enemy('Giant_bug', 50, (2,3))
+snake = Enemy('Snake', 20, (3,6))
+
+wolf = Enemy('Wolf', 30, (8,10))
