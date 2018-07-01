a5a53cd98fd5d8a5c0d4bfe66a9a7c6a5f389623
Saphiraa <40579172+Saphiraa@users.noreply.github.com>
2018-07-01 12:46:53 +0530

    Update Actions.py

--- Actions.py
+++ Actions.py
@@ -5,7 +5,7 @@ def combat(player, enemy, room=None): # Future scope: add room for floor.
 	
 	playerTurn = True
 	# The player attacks first. 
-	while(player.isAlive() and enemy.isAlive()): # If either is alive, loop continues.
+	while(player.isAlive() and enemy.isAlive()): # If both are alive, loop continues.
 
 		print('\n\n')
 
