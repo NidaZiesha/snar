4a3671d29fdded78667dbacdd29cc85e595cf8a3
Saphiraa <40579172+Saphiraa@users.noreply.github.com>
2018-07-01 15:44:29 +0530

    Update Character.py
    new gold attribute added to Player and Enemy

--- Character.py
+++ Character.py
@@ -22,12 +22,13 @@ class Player(Character):
     def setInfo(self):
         inp = input("You-Know-Who needs your name. \n\n>>")
         super(Player, self).setInfo(inp, 100)
+        self.gold = 0
         self.sp = 100
         self.invent = {}
 
 
     def info(self):
-        print(f"{self.name} hp: {self.hp} sta: {self.sp}")
+        print(f"{self.name} hp: {self.hp} gold: {self.gold} sta: {self.sp}")
 
     # Creates a key, value pair for objects added to inventory
     def addToInventory(self, obj):
@@ -54,14 +55,15 @@ class Player(Character):
 class Enemy(Character):
 
     #inventory = {'Sword': (5,15), 'Gun': (10,25)}
-    def __init__(self, name, hp, dmg_range):
+    def __init__(self, name, hp, gold, dmg_range):
     	self.name = name
     	self.hp = hp
+        self.gold = gold
     	self.dmg_range = dmg_range
     	self.invent = {}
 
     def info(self):
-        print(f"{self.name} hp: {self.hp}")
+        print(f"{self.name} hp: {self.hp} gold: {self.gold}")
 
     def attack(self, target):
         target.hp -= randint(*self.dmg_range)
